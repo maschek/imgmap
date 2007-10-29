9e8ec5726dab7c8069820c2ca5283eb398d7ba6e
adam.maschek@gmail.com <adam.maschek@gmail.com@d2e25085-9c39-0410-a79b-cf092336fea4>
2007-10-29 23:24:54 +0000

    alfonso's latest improvements + minor bugfixes

--- examples/example6_files/imgmap/jscripts/functions.js
+++ examples/example6_files/imgmap/jscripts/functions.js
@@ -2,45 +2,45 @@ var myimgmap;
 var img_obj = null;
 var map_obj = null;
 
-var editor = window.parent.InnerDialogLoaded();
+// Less boring code
+window.$ = function( id )
+{
+	return this.document.getElementById( id ) ;
+} ;
+
+var oEditor = window.parent.InnerDialogLoaded();
+var FCKConfig	= oEditor.FCKConfig ;
+
+document.write('<scr'+'ipt type="text/javascript" src="' + FCKConfig.FullBasePath + 'dialog/common/fck_dialog_common.js"></sc' + 'ript>');
+
 
 window.onload = function()
 {
 	window.parent.SetOkButton(true);
-	window.parent.SetAutoSize(true);
+//	window.parent.SetAutoSize(true);
 
 	//translate page elements
-	editor.FCKLanguageManager.TranslatePage(document);
-	editor.FCKLanguageManager.TranslateElements(document, 'A', 'innerHTML');
+	oEditor.FCKLanguageManager.TranslatePage(document);
+	oEditor.FCKLanguageManager.TranslateElements(document, 'A', 'innerHTML');
 
-	//console.log(editor);
-	img_obj = editor.FCK.Selection.GetSelectedElement();
-	
-	//console.log(imgmap);
+	img_obj = oEditor.FCK.Selection.GetSelectedElement();
 
 	//late init
 	myimgmap = new imgmap({
 		mode : "editor",
-		button_container: document.getElementById('button_container'),
+		button_container: $('button_container'),
 		imgroot: 'images/',
-		buttons : ['add','delete','html'],
+		buttons : ['delete'],
 		custom_callbacks : {
-			'onHtml' : function() {htmlShow();},
-			'onSelectArea' : function(obj) {
-				//console.log(obj);
-				myimgmap.areas[obj.aid].ahref = 'foo.bar';
-				myimgmap.areas[obj.aid].aalt = 'alt changed';
-				myimgmap._recalculate(obj.aid);
-				if (myimgmap.html_container) myimgmap.html_container.value = myimgmap.getMapHTML();
-				//console.log(obj);
-				
-				
-			}
+//			'onHtml' : function() {htmlShow();},
+			'onSelectArea' : onSelectArea,
+			'onRemoveArea'	: onRemoveArea ,
+			'onGetAreaHtml'	: onGetAreaHtml
 		},
-		html_container: document.getElementById('html_container'), 
-		pic_container: document.getElementById('pic_container'),
-		status_container: document.getElementById('status_container'),
-		form_container: document.getElementById('form_container'),
+		html_container: null, // $('html_container'), 
+		pic_container: $('pic_container'),
+		status_container: $('status_container'),
+		form_container: null, // $('form_container'),
 		bounding_box : false
 	});
 
@@ -49,39 +49,48 @@ window.onload = function()
 	myimgmap.onLoad();
 
 	myimgmap.loadImage(img_obj);
-	//console.log(myimgmap);
 
 	//check if the image has a valid map already assigned
 	var mapname = img_obj.getAttribute('usemap', 2) || img_obj.usemap ;
 	//console.log(mapname);
-	if (mapname != null && mapname != '') {
+	if ( typeof mapname == 'string' && mapname !== '') {
 		mapname = mapname.substr(1);
-		var maps = editor.FCK.EditorDocument.getElementsByTagName('MAP');
+		var maps = oEditor.FCK.EditorDocument.getElementsByTagName('MAP');
 		//console.log(maps);
 		for (var i=0; i < maps.length; i++) {
 			if (maps[i].name == mapname) {
 				map_obj = maps[i];
 				myimgmap.setMapHTML(map_obj);
 
-				document.getElementById('MapName').value = mapname ;
+				$('MapName').value = mapname ;
 				break;
 			}
 		}
 	}
 
-	hightlightMode( 'rectangle' );
-}
+	// We must set up this listener only after the current data has been read
+	myimgmap.config.custom_callbacks.onAddArea = onAddArea ;
+
+	$('btnBrowse').style.display	= FCKConfig.LinkBrowser		? '' : 'none' ;
+
+	if ( map_obj !== null )
+		setMode( 'pointer' ) ;
+	else
+		hightlightMode( 'rectangle' ) ;
+
+	RefreshSize() ;
+} ;
 
 function Ok() {
-	if (img_obj != null && img_obj.nodeName == "IMG") {
-		editor.FCKUndo.SaveUndoStep();
+	if (img_obj !== null && img_obj.nodeName == "IMG") {
+		oEditor.FCKUndo.SaveUndoStep();
 
-		if (typeof map_obj == 'undefined' || map_obj == null) {
-			map_obj = editor.FCK.EditorDocument.createElement('MAP');
+		if (typeof map_obj == 'undefined' || map_obj === null) {
+			map_obj = oEditor.FCK.EditorDocument.createElement('MAP');
 			img_obj.parentNode.appendChild(map_obj);
 		}
 
-		myimgmap.mapid = myimgmap.mapname = document.getElementById('MapName').value ;
+		myimgmap.mapid = myimgmap.mapname = $('MapName').value ;
 
 		map_obj.innerHTML = myimgmap.getMapInnerHTML();
 
@@ -101,11 +110,11 @@ function Ok() {
 
 //remove the map object and unset the usemap attribute
 function removeMap() {
-	editor.FCKUndo.SaveUndoStep();
-	if (img_obj != null && img_obj.nodeName == "IMG") {
+	oEditor.FCKUndo.SaveUndoStep();
+	if (img_obj !== null && img_obj.nodeName == "IMG") {
 		img_obj.removeAttribute('usemap', 0);
 	}
-	if (typeof map_obj != 'undefined' && map_obj != null) {
+	if (typeof map_obj != 'undefined' && map_obj !== null) {
 		map_obj.parentNode.removeChild(map_obj);
 	}
 
@@ -128,7 +137,7 @@ function toggleFieldset(fieldset, on) {
 	if (fieldset) {
 		if (fieldset.className == 'fieldset_off' || on == 1) {
 			fieldset.className = '';
-			resizeToContent();
+			RefreshSize();
 		}
 		else {
 			fieldset.className = 'fieldset_off';
@@ -136,32 +145,74 @@ function toggleFieldset(fieldset, on) {
 	}
 }
 
-function htmlShow() {
-	toggleFieldset(document.getElementById('fieldset_html'), 1);
-	document.getElementById('html_container').focus();
+// We need to store here the area id because if the user clicks on an area, 
+// then the onSelectArea event will fire before the onchange or onblur of the editing inputs
+var currentAreaId = null ;
+
+// An area has been selected in the image
+function onSelectArea(obj) {
+	$( 'properties' ).style.visibility = '';
+
+	updateAreaValues() ;
+
+	currentAreaId = obj.aid ;
+	$( 'txtUrl' ).value = obj.ahref ;
+	$( 'cmbTarget' ).value = obj.atarget ;
+	$( 'txtAlt' ).value = obj.aalt ;
+	$( 'txtAttTitle' ).value = obj.atitle ;
 }
 
-function resizeToContent() {
-//alert('b');
-	if (document.all) {
-			try { window.resizeTo(10, 10); } catch (e) {}
+// A new area has been added
+function onAddArea(id) {
+	$( 'properties' ).style.visibility = '';
 
-			elm = document.body;
-			width = elm.offsetWidth;
-			height = elm.offsetHeight;
-			dx = (elm.scrollWidth - width) + 4;
-			dy = elm.scrollHeight - height;
-			try { window.resizeBy(dx, dy); } catch (e) {}
-		} else {
-			window.scrollBy(1000, 1000);
-			if (window.scrollX > 0 || window.scrollY > 0) {
-				window.resizeBy(window.innerWidth * 2, window.innerHeight * 2);
-				window.sizeToContent();
-				window.scrollTo(0, 0);
-				x = parseInt(screen.width / 2.0) - (window.outerWidth / 2.0);
-				y = parseInt(screen.height / 2.0) - (window.outerHeight / 2.0);
-				window.moveTo(x, y);
-			}
+	updateAreaValues() ;
+
+	currentAreaId = id ;
+	$( 'txtUrl' ).value = '' ;
+	$( 'cmbTarget' ).value = '' ;
+	$( 'txtAlt' ).value = '' ;
+	$( 'txtAttTitle' ).value = '' ;
+}
+
+function onRemoveArea()
+{
+	currentAreaId = null ;
+	$( 'properties' ).style.visibility = 'hidden' ;
+}
+
+function updateAreaValues()
+{
+	if (currentAreaId !== null)
+	{
+		myimgmap.areas[currentAreaId].ahref = $( 'txtUrl' ).value ;
+		myimgmap.areas[currentAreaId].aalt = $( 'txtAlt' ).value ;
+		myimgmap.areas[currentAreaId].atitle = $( 'txtAttTitle' ).value ;
+		myimgmap._recalculate(currentAreaId);
+	}
+}
+
+// We use our own resizing because the default doesn't take into account Standards rendering mode.
+function RefreshSize()
+{
+		var oInnerDoc = document ;
+
+		var iFrameHeight ;
+		if ( document.all )
+			iFrameHeight = oInnerDoc.documentElement.offsetHeight ;
+		else
+			iFrameHeight = window.innerHeight ;
+
+		var iInnerHeight = oInnerDoc.body.scrollHeight ;
+
+		var iDiff = iInnerHeight - iFrameHeight ;
+
+		if ( iDiff !== 0 )
+		{
+			if ( document.all )
+				window.parent.dialogHeight = ( parseInt( window.parent.dialogHeight, 10 ) + iDiff ) + 'px' ;
+			else
+				window.parent.resizeBy( 0, iDiff ) ;
 		}
 }
 
@@ -184,6 +235,84 @@ function hightlightMode(mode) {
 		previousModeImg.className = '';
 
 	// Highlight new mode
-	previousModeImg = document.getElementById( 'img' + mode );
+	previousModeImg = $( 'img' + mode );
 	previousModeImg.className = 'ActiveMode' ;
+}
+
+function onGetAreaHtml(area)
+{
+	var html = '<area shape="' + area.shape + '"' +
+							' coords="' + area.lastInput + '"' ;
+
+	if (area.aalt && area.aalt!='') html+= ' alt="' + area.aalt + '"' ;
+	if (area.atitle && area.atitle!='') html+= ' title="' + area.atitle + '"' ;
+	if (area.ahref && area.ahref!='') html+= ' href="' +	area.ahref + '" _fcksavedurl="' +	area.ahref + '"' ;
+	if (area.atarget && area.atarget!='') html+= ' target="' + area.atarget + '"' ;
+						
+	html+='/>';
+	return html;
+}
+
+/* edit the properties of an area */
+
+function SetUrl(value)
+{
+	var id = currentAreaId ;
+	if ( id !== null)
+	{
+		myimgmap.areas[id].ahref = value ;
+		myimgmap._recalculate(id);
+	}
+}
+
+
+function SetTarget(value)
+{
+	var id = currentAreaId ;
+	if ( id !== null)
+	{
+		myimgmap.areas[id].atarget = value ;
+		myimgmap._recalculate(id);
+	}
+}
+
+
+function SetAlt(value)
+{
+	var id = currentAreaId ;
+	if ( id !== null)
+	{
+		myimgmap.areas[id].aalt = value ;
+		myimgmap._recalculate(id);
+	}
+}
+
+
+function SetTitle(value)
+{
+	var id = currentAreaId ;
+	if ( id !== null)
+	{
+		myimgmap.areas[id].atitle = value ;
+		myimgmap._recalculate(id);
+	}
+}
+
+
+/* Browse server */
+
+function BrowseServer()
+{
+	OpenFileBrowser(
+		FCKConfig.LinkBrowserURL,
+		FCKConfig.LinkBrowserWindowWidth,
+		FCKConfig.LinkBrowserWindowHeight ) ;
+}
+
+function SetUrl( url, width, height, alt )
+{
+	GetE('txtUrl').value = url ;
+
+	if ( alt )
+		GetE('txtAlt').value = alt;
 }
