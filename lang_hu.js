60d508521e40b289a8adfeeb3cc5a2e2132bb687
adam.maschek@gmail.com <adam.maschek@gmail.com@d2e25085-9c39-0410-a79b-cf092336fea4>
2007-12-28 18:01:40 +0000

    jslint compliance, highlight improvements, hungarian lang, auto language recognition...example 6 half-broken in this one!

---
+++ lang_hu.js
@@ -0,0 +1,37 @@
+//hungarian string resources
+
+var imgmapStrings = {
+
+READY                   : 'Kész.',
+PREVIEW_MODE            : 'Előnézet.',
+DESIGN_MODE             : 'Tervezés nézet. Rajzoljon új területeket vagy módosítsa a már meglévőket.',
+
+HINT_ADD                : 'Új terület hozzáadása',
+HINT_DELETE             : 'Kiválasztott terület törlése',
+HINT_PREVIEW            : 'Előnézet',
+HINT_HTML               : 'HTML kód megjelenítése',
+HINT_CLIPBOARD          : 'Vágólapra másolás',
+
+RECTANGLE_DRAW          : 'Téglalap rajzolás. Tartsa lenyomva a SHIFT billentyűt négyzet rajzoláshoz.',
+RECTANGLE_MOVE          : 'Téglalap mozgatás',
+RECTANGLE_RESIZE_TOP    : 'Téglalap átméretezés',
+RECTANGLE_RESIZE_RIGHT  : 'Téglalap átméretezés',
+RECTANGLE_RESIZE_BOTTOM : 'Téglalap átméretezés',
+RECTANGLE_RESIZE_LEFT   : 'Téglalap átméretezés',
+
+SQUARE_DRAW             : 'Kör rajzolás',
+SQUARE2_DRAW            : 'Négyzet rajzolás. Engedje el a SHIFT billentyűt téglalap rajzoláshoz.',
+SQUARE_MOVE             : 'Kör mozgatás',
+SQUARE_RESIZE_TOP       : 'Kör átméretezés',
+SQUARE_RESIZE_RIGHT     : 'Kör átméretezés',
+SQUARE_RESIZE_BOTTOM    : 'Kör átméretezés',
+SQUARE_RESIZE_LEFT      : 'Kör átméretezés',
+
+POLYGON_DRAW            : 'Sokszög rajzolás. SHIFT + kattintás az utolsó pontnál.',
+POLYGON_MOVE            : 'Sokszög mozgatás',
+
+ERR_EXCANVAS_LOAD       : 'Az Excanvas nem töltődött be rendesen. Próbálja meg frissíteni az oldalt.',
+ERR_INVALID_COORDS      : 'Érvénytelen koordináták.'
+
+};
+//alert('lang loaded');
