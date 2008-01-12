1f47f7d7226f27bb7aac2c52cb1520816ee36a45
adam.maschek@gmail.com <adam.maschek@gmail.com@d2e25085-9c39-0410-a79b-cf092336fea4>
2008-01-12 22:46:58 +0000

    reviewed & renewed tiny extension, added german language, fixed example 1 upload paths

---
+++ examples/example3_files/tinymce/jscripts/tiny_mce/plugins/imgmap/jscripts/functions.js
@@ -0,0 +1,90 @@
+var myimgmap;
+var editor = null;
+var img_obj = null;
+var map_obj = null;
+
+function init() {
+
+	tinyMCE.setWindowArg('mce_windowresize', true);
+
+	editor = tinyMCE.getInstanceById(tinyMCE.getWindowArg('editor_id'));
+	img_obj = editor.selection.getFocusElement();
+
+	//late init
+	myimgmap = new imgmap({
+		mode : "editor",
+		button_container: document.getElementById('button_container'),
+		imgroot: 'images/',
+		buttons : ['add','delete','html'],
+		custom_callbacks : {
+			'onHtml' : function() {htmlShow();}
+		},
+		pic_container: document.getElementById('pic_container'),
+		html_container: document.getElementById('html_container'),
+		status_container: document.getElementById('status_container'),
+		form_container: document.getElementById('form_container'),
+		bounding_box : false
+	});
+
+	
+	//we need this to load languages
+	myimgmap.onLoad();
+
+	myimgmap.loadImage(img_obj);
+	//console.log(myimgmap);
+	
+	//check if the image has a valid map already assigned
+	var mapname = img_obj.getAttribute('usemap', 2) || img_obj.usemap ;
+	//console.log(mapname);
+	if (mapname != null && mapname != '') {
+		mapname = mapname.substr(1);
+		var maps = editor.contentWindow.document.getElementsByTagName('MAP');
+		//console.log(maps);
+		for (var i=0; i < maps.length; i++) {
+			// IE doesn't return name?
+			if (maps[i].name == mapname || maps[i].id == mapname) {
+				map_obj = maps[i];
+				myimgmap.setMapHTML(map_obj);
+				break;
+			}
+		}
+	}
+}
+
+function updateAction() {
+	if (img_obj != null && img_obj.nodeName == "IMG") {
+		tinyMCEPopup.execCommand("mceBeginUndoLevel");
+
+		if (typeof map_obj == 'undefined' || map_obj == null) {
+			map_obj = editor.contentWindow.document.createElement('MAP');
+			img_obj.parentNode.appendChild(map_obj);
+		}
+
+		map_obj.innerHTML = myimgmap.getMapInnerHTML();
+		map_obj.name = myimgmap.getMapName();
+		map_obj.id   = myimgmap.getMapId();
+		
+		img_obj.setAttribute('usemap', "#" + myimgmap.getMapName(), 0);
+		//img_obj.setAttribute('border', '0');
+		
+		tinyMCEPopup.execCommand("mceEndUndoLevel");
+	}
+	tinyMCEPopup.close();
+}
+
+function cancelAction() {
+	tinyMCEPopup.close();
+}
+
+//remove the map object and unset the usemap attribute
+function removeAction() {
+	tinyMCEPopup.execCommand("mceBeginUndoLevel");
+	if (img_obj != null && img_obj.nodeName == "IMG") {
+		img_obj.removeAttribute('usemap', 0);
+	}
+	if (typeof map_obj != 'undefined' && map_obj != null) {
+		map_obj.parentNode.removeChild(map_obj);
+	}
+	tinyMCEPopup.execCommand("mceEndUndoLevel");
+	tinyMCEPopup.close();
+}
---
+++ lang_de.js
@@ -0,0 +1,37 @@
+//german string resources by Iwan Fux
+
+var imgmapStrings = {
+
+READY                   : 'Bereit',
+PREVIEW_MODE            : 'Vorschau Modus. Image Map testen.',
+DESIGN_MODE             : 'Design Modus. Neue Formen hinzufügen oder bestehende bearbeiten.',
+
+HINT_ADD                : 'Neuen Bereich hinzufügen',
+HINT_DELETE             : 'Ausgewählte Bereiche löschen',
+HINT_PREVIEW            : 'Vorschau Image Map',
+HINT_HTML               : 'Image Map HTML erhalten',
+HINT_CLIPBOARD          : 'In Zwischenablage kopieren',
+
+RECTANGLE_DRAW          : 'Viereck wird gezeichnet. SHIFT drücken für Quadrat Modus.',
+RECTANGLE_MOVE          : 'Viereck wird bewegt',
+RECTANGLE_RESIZE_TOP    : 'Viereckhöhe wird verändert',
+RECTANGLE_RESIZE_RIGHT  : 'Viereckbreite wird verändert',
+RECTANGLE_RESIZE_BOTTOM : 'Viereckhöhe wird verändert',
+RECTANGLE_RESIZE_LEFT   : 'Viereckbreite wird verändert',
+
+SQUARE_DRAW             : 'Kreis wird gezeichnet',
+SQUARE2_DRAW            : 'Quadrat wird gezeichnet. SHIFT loslassen für Viereck Modus.',
+SQUARE_MOVE             : 'Kreis wird bewegt',
+SQUARE_RESIZE_TOP       : 'Kreisgrösse wird verändert',
+SQUARE_RESIZE_RIGHT     : 'Kreisgrösse wird verändert',
+SQUARE_RESIZE_BOTTOM    : 'Kreisgrösse wird verändert',
+SQUARE_RESIZE_LEFT      : 'Kreisgrösse wird verändert',
+
+POLYGON_DRAW            : 'Polygon wird gezeichnet. Für den letzten Kontrollpunkt SHIFT + Klicken.',
+POLYGON_MOVE            : 'Polygon wird bewegt',
+
+ERR_EXCANVAS_LOAD       : 'Excanvas nicht korrekt geladen. Versuch Seite neu zu laden.',
+ERR_INVALID_COORDS      : 'Ungültige Koordinaten.'
+
+};
+//alert('lang loaded');
