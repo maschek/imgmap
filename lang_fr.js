786f0a36857ecbeb04825c6d7a01fa2341074898
adam.maschek@gmail.com <adam.maschek@gmail.com@d2e25085-9c39-0410-a79b-cf092336fea4>
2008-02-18 01:24:17 +0000

    AIR improvements, new langs

---
+++ lang_fr.js
@@ -0,0 +1,37 @@
+//english string resources - Paul Cazes
+
+var imgmapStrings = {
+
+READY                   : "Pret",
+PREVIEW_MODE            : "Mode pre-visualisation. Test de l'image",
+DESIGN_MODE             : "Mode design. Ajou de nouvelles formes ou modification des existantes",
+
+HINT_ADD                : "Ajouter une nouvelle zone",
+HINT_DELETE             : "Detruire la zone selectionnee",
+HINT_PREVIEW            : "Pre-visualiser l'image",
+HINT_HTML               : "Obtenir l'image HTML",
+HINT_CLIPBOARD          : "Copier dans le presse papier",
+
+RECTANGLE_DRAW          : "Dessiner un rectangle. Maintenir enfoncer la touche SHIFT pour le mode carre",
+RECTANGLE_MOVE          : "Deplacer le rectangle",
+RECTANGLE_RESIZE_TOP    : "Redimensionner le rectangle",
+RECTANGLE_RESIZE_RIGHT  : "Redimensionner le rectangle",
+RECTANGLE_RESIZE_BOTTOM : "Redimensionner le rectangle",
+RECTANGLE_RESIZE_LEFT   : "Redimensionner le rectangle",
+
+SQUARE_DRAW             : "Dessiner un cercle",
+SQUARE2_DRAW            : "Dessiner un carre. Relacher la touche SHIFT pour le mode rectangle",
+SQUARE_MOVE             : "Deplacer le carre",
+SQUARE_RESIZE_TOP       : "Redimensionner le carre",
+SQUARE_RESIZE_RIGHT     : "Redimensionner le carre",
+SQUARE_RESIZE_BOTTOM    : "Redimensionner le carre",
+SQUARE_RESIZE_LEFT      : "Redimensionner le carre",
+
+POLYGON_DRAW            : "Dessiner un polygone. Utiliser SHIFT + Click pour le dernier point de controle",
+POLYGON_MOVE            : "Deplacer le polygone",
+
+ERR_EXCANVAS_LOAD       : "Erreur de chargement Excanvas. Essayez de rafraichir la page",
+ERR_INVALID_COORDS      : "Coordonnees invalides"
+
+};
+//alert('lang loaded');
