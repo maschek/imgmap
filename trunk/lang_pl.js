e187f73fa2034f8ad2a08a98b2ac6fdf56058274
adam.maschek@gmail.com <adam.maschek@gmail.com@d2e25085-9c39-0410-a79b-cf092336fea4>
2011-05-27 17:12:18 +0000

    polish string resources

---
+++ trunk/lang_pl.js
@@ -0,0 +1,37 @@
+//polish string resources - Rafał P. Kowalski
+
+var imgmapStrings = {
+
+READY                   : 'Gotowe',
+PREVIEW_MODE            : 'Tryb podglądu. Przetestuj swój image map.',
+DESIGN_MODE             : 'Tryb projektowania. Dodaj nowe kształty lub zmień istniejące.',
+
+HINT_ADD                : 'Dodaj nowy obszar',
+HINT_DELETE             : 'Skasuj wybrany obszar',
+HINT_PREVIEW            : 'Otwórz podgląd',
+HINT_HTML               : 'Pozyskaj HTML',
+HINT_CLIPBOARD          : 'Kopiuj do schowka',
+
+RECTANGLE_DRAW          : 'Kreślenie prostokąta. Wciśnij i przytrzymaj SHIFT w celu kreślenia kwadratu.',
+RECTANGLE_MOVE          : 'Przemieszczanie prostokąta',
+RECTANGLE_RESIZE_TOP    : 'Zmiana rozmiarów prostokąta',
+RECTANGLE_RESIZE_RIGHT  : 'Zmiana rozmiarów prostokąta',
+RECTANGLE_RESIZE_BOTTOM : 'Zmiana rozmiarów prostokąta',
+RECTANGLE_RESIZE_LEFT   : 'Zmiana rozmiarów prostokąta',
+
+SQUARE_DRAW             : 'Kreślenie okręgu',
+SQUARE2_DRAW            : 'Kreślenie kwadratu. Puść SHIFT w celu kreślenia prostokąta.',
+SQUARE_MOVE             : 'Przemieszczanie okręgu',
+SQUARE_RESIZE_TOP       : 'Zmiana rozmiarów okręgu',
+SQUARE_RESIZE_RIGHT     : 'Zmiana rozmiarów okręgu',
+SQUARE_RESIZE_BOTTOM    : 'Zmiana rozmiarów okręgu',
+SQUARE_RESIZE_LEFT      : 'Zmiana rozmiarów okręgu',
+
+POLYGON_DRAW            : 'Kreślenie wielokąta. Wciśnij SHIFT i kliknij w celu dodania ostatniego punktu.',
+POLYGON_MOVE            : 'Przemieszczanie wielokąta',
+
+ERR_EXCANVAS_LOAD       : 'Biblioteka Excanvas nie została poprawnie załadowana. Spróbuj odświeżyć stronę.',
+ERR_INVALID_COORDS      : 'Nieprawidłowe współrzędne.'
+
+};
+//alert('lang loaded');
