9a1c040eaa59c9118943b54030d35e3cdd95cee4
adam.maschek@gmail.com <adam.maschek@gmail.com@d2e25085-9c39-0410-a79b-cf092336fea4>
2011-05-27 17:05:44 +0000

    added latvian translation

---
+++ trunk/examples/example_fck_files/imgmap/lang/lv.js
@@ -0,0 +1,27 @@
+
+
+FCKLang.imgmapBtn			= 'Ievietot/Labot attēlkarti';
+FCKLang.imgmapBtnRemove			= 'Noņemt attēlkarti';
+FCKLang.imgmapDlgTitle		= 'Attēlkartes Redaktors';
+FCKLang.imgmapDlgName		= 'ImageMapEditor';
+
+FCKLang.msgImageNotSelected = 'Ir jāizvēlas attēls pirms izmantot šo dialogu' ;
+
+FCKLang.imgmapNoLabel = 'Nav iezīmes' ;
+FCKLang.imgmapLabelNumber = 'Iezīme ar skaitļiem' ;
+FCKLang.imgmapLabelAlt = 'Iezīme ar alternatīvu tekstu' ;
+FCKLang.imgmapLabelHref = 'Iezīme ar hiperlinku' ;
+FCKLang.imgmapLabelTitle = 'Iezīme ar title' ;
+FCKLang.imgmapLabelCoords = 'Iezīme ar koordinatēm' ;
+FCKLang.imgmapLabelZoom = 'Tālummaiņas' ;
+
+FCKLang.imgmapMap = 'Karte' ;
+FCKLang.imgmapMapName = 'Kartes vārds' ;
+FCKLang.imgmapMapAreas = 'Attēlkartes Apgabali' ;
+
+FCKLang.imgmapPointer = 'Norāde' ;
+FCKLang.imgmapRectangle = 'Taisnstūris' ;
+FCKLang.imgmapCircle = 'Aplis' ;
+FCKLang.imgmapPolygon = 'Poligons' ;
+FCKLang.imgmapDeleteArea = 'Dzēst iezīmēto apgabalu' ;
+
