702e9f0a9ca4b8df516e6df21643c96fc43ed603
Ferdi ÖZKAN <ferdi.ozkan@msn.com>
2016-07-27 15:18:50 +0300

    added turkish translation

---
+++ lang_tr.js
@@ -0,0 +1,37 @@
+//english string resources
+
+var imgmapStrings = {
+
+READY                   : 'Hazır',
+PREVIEW_MODE            : 'Önizleme modu. Hazırladığınız haritayı test edin.',
+DESIGN_MODE             : 'Tasarım modu. Yeni şekiller ekleyebilir veya var olanları güncelleye bilirsiniz.',
+
+HINT_ADD                : 'Yeni alan ekle',
+HINT_DELETE             : 'Seçili alanı sil',
+HINT_PREVIEW            : 'Resim haritası önizleme',
+HINT_HTML               : 'HTML kodunu al',
+HINT_CLIPBOARD          : 'Panoya kopyala',
+
+RECTANGLE_DRAW          : 'Dikdörtgen çizimi. Kare için SHIFT tuşuna basılı tutun.',
+RECTANGLE_MOVE          : 'Hareketli dikdörtgen',
+RECTANGLE_RESIZE_TOP    : 'Dikdörtgeni yeniden boyutlandırma',
+RECTANGLE_RESIZE_RIGHT  : 'Dikdörtgeni yeniden boyutlandırma',
+RECTANGLE_RESIZE_BOTTOM : 'Dikdörtgeni yeniden boyutlandırma',
+RECTANGLE_RESIZE_LEFT   : 'Dikdörtgeni yeniden boyutlandırma',
+
+SQUARE_DRAW             : 'Daire çizimi',
+SQUARE2_DRAW            : 'Kare çizimi. Dikdörtgen çizimi için SHIFT tuşunu serbest bırakın.',
+SQUARE_MOVE             : 'Hareketli Daire',
+SQUARE_RESIZE_TOP       : 'Daireyi yeniden boyutlandırma',
+SQUARE_RESIZE_RIGHT     : 'Daireyi yeniden boyutlandırma',
+SQUARE_RESIZE_BOTTOM    : 'Daireyi yeniden boyutlandırma',
+SQUARE_RESIZE_LEFT      : 'Daireyi yeniden boyutlandırma',
+
+POLYGON_DRAW            : 'Çokgen çizimi. Çizimi tamamlamak için SHIFT tuşuyla beraber tıklayın.',
+POLYGON_MOVE            : 'Hareketli .okgen',
+
+ERR_EXCANVAS_LOAD       : 'Excanvas düzgün yüklenemedi. Sayfayı yeniden yükleyin',
+ERR_INVALID_COORDS      : 'Hatalı koordinatlar.'
+
+};
+//alert('lang loaded');
